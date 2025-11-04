a1411941de3b065bdc9c65a20910f9d462cf4627
janith-t <janith.tw@gmail.com>
2025-11-04 11:08:58 +0530

    initial commit

---
+++ src/main/java/lk/janith/restetlexample/RestEtlExampleApplication.java
@@ -0,0 +1,13 @@
+package lk.janith.restetlexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEtlExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEtlExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/janith/restetlexample/RestEtlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.janith.restetlexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestEtlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
